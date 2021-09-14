70d64849f54d72b79b63607b8c7f9cfdb14ba533
Le Trong Hieu <eotihvn@gmail.com>
2021-09-14 17:49:41 +0700

    abc

--- SCSE-DASHBOARD/pages/DAO/Vietnam.js
+++ SCSE-DASHBOARD/pages/DAO/Vietnam.js
@@ -1,5 +1,6 @@
 
 window.addEventListener('load', loadData)
+window.addEventListener('load', loadData2)
 
 async function loadData() {
     fetch("https://owsnews.herokuapp.com/covid")
@@ -7,24 +8,21 @@ async function loadData() {
             return response.json();
         })
         .then(function (response) {
-            document.getElementById("imgLogo").src = response.Logo;
-            $('#TenCongTy').text(response.Name);
-            $('#lv1').text(response.Field);
-            $('#SoDienThoai').text(response.Phone);
-            $('#mail').text(response.Email);
-            $('#Fanpage2').text(response.Fanpage);
-            $('#Youtube2').text(response.Youtube);
+            console.log(response.data)
+            var tong = 0;
+            for (let i = 0; i < response.data.length; i++) {
+                tong += response.data[i].tong_nhiem;
+            }
+        })
+}
+async function loadData2() {
+    fetch("https://static.pipezero.com/covid/data.json")
+        .then(function (response) {
+            return response.json();
+        })
+        .then(function (response) {
+            console.log(response.total)
 
-
-            $('#ID').val(response.ID);
-            document.getElementById("LogoToChuc").src = response.Logo;
-            $('#Name').val(response.Name);
-            $('#Field').val(response.Field);
-            $('#Phone').val(response.Phone);
-            $('#Email').val(response.Email);
-            $('#Address').val(response.Address);
-            $('#Fanpage').val(response.Fanpage);
-            $('#Youtube').val(response.Youtube);
         })
 }
 
