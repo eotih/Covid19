7e25e7946601cf4cecc501e21a47159566304734
Le Trong Hieu <eotihvn@gmail.com>
2021-09-15 18:53:21 +0700

    đưa tổng số ca lên

--- Dashboard-Covid19/data.js
+++ Dashboard-Covid19/data.js
@@ -1,6 +1,15 @@
 (function ($) {
   'use strict';
   $(function () {
+    fetch("https://static.pipezero.com/covid/data.json")
+    .then(function (response){
+      return response.json();
+    })
+    .then(function (response){
+      $('#tongsocathegioi').text(response.total.world.cases.toLocaleString() +" Ca nhiễm");
+      $('#tongsocahoiphuctrongnuoc').text(response.total.internal.recovered.toLocaleString() +" Ca hồi phục");
+      $('#tongsocahoiphucthegioi').text(response.total.world.recovered.toLocaleString() +" Ca hồi phục");
+    })
     fetch("https://owsnews.herokuapp.com/covid")
       .then(function (response) {
         return response.json();
@@ -13,8 +22,8 @@
           $tongcanhiem += Number(response.data[i].tong_nhiem.replace(/\./g, ''));
         }
         $('#nguon').text(response.source_covid);
-        $('#tongsocanhiem').text($tongcanhiem +" Ca nhiễm");
-        $('#tongsotuvong').text($tongtuvong +" Ca tử vong");
+        $('#tongsocanhiem').text($tongcanhiem.toLocaleString() +" Ca nhiễm");
+        $('#tongsotuvong').text($tongtuvong.toLocaleString() +" Ca tử vong");
         var $hieu = response.data;
         if ($("#js-grid").length) {
           $("#js-grid").jsGrid({
